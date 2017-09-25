81e7cc5a9207d14a1c86111c3aed1633b79725f0
youngky hermanto <yhermanto@gmail.com>
2017-09-25 22:25:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,9 @@ function sayHiToGrandma(string) {
   else if ( string == string.toUpperCase()) {
     return "YES INDEED!"
   }
-  /*
+  
   else if (string == "I love you, Grandma.") {
     return "I love you, too."
   }
 
-  */
 }
