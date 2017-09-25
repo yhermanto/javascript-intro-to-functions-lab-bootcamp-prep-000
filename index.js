13054e8bf158f64916f73052c956ab9a174d9dad
youngky hermanto <yhermanto@gmail.com>
2017-09-25 22:20:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 }
-/*
+
 function sayHiToGrandma(string) {
   if(string == string.toLowerCase()) {
     return "I can't hear you!"
   }
+  /*
   else if ( string == string.toUpperCase()) {
     return "YES INDEED!"
   }
